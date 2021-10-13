72095b3e51a490f0c808dfd04fd54d68cbc6efa9
an-agha <anaghanisakar@gmail.com>
2021-10-13 07:18:15 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function showNav(){
+    if(navbarNav.style.display==="none"){
+        document.getElementById('navbarNav').style.display='flex';
+    }
+    else{
+        document.getElementById('navbarNav').style.display='none'; 
+    }
+}
